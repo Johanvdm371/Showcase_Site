100ee456007399d2b541cdc8bdcd4f15f68606b3
Johanvdm371 <johankerkmusiekant@gmail.com>
2019-09-24 11:14:53 +1000

    first commit of full page

---
+++ README.txt
@@ -0,0 +1,3 @@
+***************
+This is my website. It's listed here so the source code is also available. All coding is my own unless stated otherwise by means of comments in the source code.
+***************
---
+++ resources/js/main.js
@@ -0,0 +1,72 @@
+const showButton = document.querySelector('.showbutton');
+const webButton = document.querySelector('.webbutton');
+
+//Popup trigger for proficiency icons on home page
+function scrollAppearProficiencies() {
+    const appearingItem1 = document.querySelector('.proficientbox1');
+    const itemTop1 = appearingItem1.getBoundingClientRect().top;
+    const appearingItem2 = document.querySelector('.proficientbox2');
+    const itemTop2 = appearingItem2.getBoundingClientRect().top;
+    const appearingItem3 = document.querySelector('.proficientbox3');
+    const itemTop3 = appearingItem3.getBoundingClientRect().top;
+    const appearingItem4 = document.querySelector('.proficientbox4');
+    const itemTop4 = appearingItem4.getBoundingClientRect().top;
+    const scrollHeight = window.innerHeight / 1.15;
+
+    if (itemTop1 < scrollHeight) {
+        appearingItem1.classList.add('appear');
+    };
+    if (itemTop2 < scrollHeight) {
+        appearingItem2.classList.add('appear');
+    };
+    if (itemTop3 < scrollHeight) {
+        appearingItem3.classList.add('appear');
+    };
+    if (itemTop4 < scrollHeight) {
+        appearingItem4.classList.add('appear');
+    };
+}
+
+//Smooth scroll to section for showcase page
+function smoothScroll(section, duration) {
+    let sect = document.querySelector(section);
+    let sectPosition = sect.getBoundingClientRect().top;
+    let startPosition = window.pageYOffset;
+    let distance = sectPosition - startPosition;
+    let startTime = null;
+
+    function scrollAnimation(currentTime) {
+        if (startTime === null) {
+            startTime = currentTime;
+        }
+        let timeElapsed = currentTime - startTime;
+        let animate = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, animate);
+        if (timeElapsed < duration) {
+            requestAnimationFrame(scrollAnimation);
+        }
+    };
+
+    //the function below is from http://www.gizma.com/easing/
+    function ease(t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
+        t--;
+        return -c/2 * (t*(t-2) - 1) + b;
+    };
+
+    requestAnimationFrame(scrollAnimation);
+}
+
+//event listeners triggered per page
+if (document.body.classList.contains("homepage")) {
+    window.addEventListener('scroll', scrollAppearProficiencies);
+}
+if (document.body.classList.contains("showcase")) {
+    showButton.addEventListener('click', function(){
+        smoothScroll('.showpieces', 1000);
+    });
+    webButton.addEventListener('click', function(){
+        smoothScroll('.websites', 1000);
+    });
+}
